dadc9f12570a2901918584a6a6508b69f8dbd35b
Eliane Regina Nicácio Mendes <elianemendes@Elianes-MacBook-Air.local>
2022-10-25 11:21:17 -0300

    feat: initial commit

---
+++ Swift_Style_Guide.playground/Contents.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+
+//MARK: - Properties
+
+var anInteger = 29
+var aDouble = 3.1416
+var aString = "Hello Word"
+
+var CGFloat: CGFloat = 44.0
+var aFloat: Float = 44.0
+
+var corFavorita = UIColor.orange // RUIM
+var favoriteColor = UIColor.orange
+var favouriteColor = UIColor.orange // RUIM
+
+
+// Colocar cada elemento do array em uma linha, com virgula no final
+var someArray = [
+    "a",
+    "b",
+]
+
+let x = 1
+let y = 2
+let z = 3
+
+/// Description: / + cmd / para incluir a documentação
+///     paramExterno1:
+///     paramExterno2:
+func someMethod(paramExterno1 paraInterno1: String, paramExterno2 paraInterno2: String) {
+    // Intencionally not implemented
+    // Alinhar: cmd + { - esquerda
+    //          cmd + } - direita
+}
+
+// Privacidade
+// Mais abertos e acessíveis
+// open
+// public
+
+// Mais restritos
+// internal
+// private
+// fileprivate
---
+++ ViewCode/Monalisa/Monalisa/MonalisaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonalisaApp.swift
+//  Monalisa
+//
+//  Created by Eliane Regina Nicácio Mendes on 18/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MonalisaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
